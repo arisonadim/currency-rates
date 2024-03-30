9f76e4ce5bdefb2cc38c3caffeae06ef8b748e39
Dmitry Ermilov <ermilov.dmitry@gmail.com>
2024-03-30 21:29:03 +0700

    [fix] typo

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import 'Styles/style.scss'
-import App from './app/App.vue'
+import App from './app/index.vue'
 
-const pinia = createPinia
+const pinia = createPinia()
 const app = createApp(App)
 
 app.use(pinia).mount('#app')
