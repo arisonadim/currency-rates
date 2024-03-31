79eeb0d3be23b3981c1757e2af1c04f655af715b
Dmitry Ermilov <ermilov.dmitry@gmail.com>
2024-03-31 12:44:16 +0700

    [add] rate trend

--- src/pages/rates-list/index.vue
+++ src/pages/rates-list/index.vue
@@ -15,18 +15,21 @@ onMounted(() => {
     <input type="text" v-model="str">
     <div class="rates-list__table" v-if="ratesStore.rates.length">
       <div>Заголовок</div>
-      <div>{{  str  }}</div>
       <div
         v-for="item in ratesStore.filterRates(str)"
         :key="item?.details.ID"
         class="rates-list__item"
       >
         {{ item.currency }} - {{ item.details.NumCode }} -
-        {{ item.details.Value }}
+        {{ item.details.Value }} - {{ item.details.Value >= item.details.Previous ? '&#8599;' :  '&#8600;' }} 
+        {{ Math.round((item.details.Value-item.details.Previous) * 1000) / 1000 }}
         <img
           :src="`https://wise.com/public-resources/assets/flags/rectangle/${item.currency.toLowerCase()}.png`"
           :alt="`${item.currency} flag`"
         />
+      </div>
+      <div class="rates-list__item" v-if="!ratesStore.filterRates(str).length">
+        Нет подходящих результатов ¯\_(ツ)_/¯ 
       </div>
     </div>
     <div v-else>loading ...</div>
