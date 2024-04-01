e04f3526e1be56b2654046a9225c111c210bcecb
Dmitry Ermilov <ermilov.dmitry@gmail.com>
2024-04-01 21:40:04 +0700

    [fix] base url prefix github pages

--- src/app/providers/router.ts
+++ src/app/providers/router.ts
@@ -4,10 +4,12 @@ import Rates from 'Pages/rates-list'
 import Calculator from 'Pages/calculator'
 import NotFound from 'Pages/404'
 
+const isProd = import.meta.env.PROD
+const prefix = isProd ? '/currency-rates' : ''
+
 const routes = [
-  { path: '/', name: 'Home', component: Rates },
-  { path: '/calc', name: 'Calc', component: Calculator },
-  // { path: '/currency-rates', redirect: { name: 'Home' } },
+  { path: `${prefix}/`, name: 'Home', component: Rates },
+  { path: `${prefix}/calc`, name: 'Calc', component: Calculator },
   { path: '/:NotFound(.*)', name: 'NotFound', component: NotFound },
 ]
 
