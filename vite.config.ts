84543c2047fd491a9d6d66c69f326c7641e0410a
Dmitry Ermilov <ermilov.dmitry@gmail.com>
2024-04-02 13:21:02 +0700

    [add] vitest conf and dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config';
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  test: {
+    environment: "jsdom",
+  },
   base: process.env.NODE_ENV === 'production' ? '/currency-rates/' : '/',
   resolve: {
     alias: {
